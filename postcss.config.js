bb0e8a2c4b0220c02dabff155667079a36733249
jonheslop <jon.heslop@pusher.com>
2020-11-20 11:15:50 +0000

    Do CSS like other repos
    I need to make an npm module or something that all the projects can share to reduce duplication

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,18 @@
-const cssnext = require("postcss-cssnext");
 const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
   plugins: [
     require("postcss-import"),
     require("postcss-nested"),
-    require("postcss-easing-gradients"),
-    cssnext({
-      features: {
-        customProperties: {
-          preserve: true,
-        },
+    require("postcss-each"),
+    require("postcss-colour-functions"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-custom-media"),
+    require("postcss-preset-env")({
+      autoprefixer: {
+        flexbox: "no-2009",
       },
+      stage: 3,
     }),
     ...(process.env.NODE_ENV === "production"
       ? [
