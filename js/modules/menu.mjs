fc18a5f61de38e4e0fe2efec1161b6a07668c91a
jonheslop <jon.heslop@pusher.com>
2020-12-03 11:29:33 +0000

    Add JS for docs nav on mobile

--- js/modules/menu.mjs
+++ js/modules/menu.mjs
@@ -1,13 +1,25 @@
 export default () => {
-  const menuButtons = [].slice.call(
-    document.querySelectorAll(".menu-toggle")
-  );
+  const menuButtons = [].slice.call(document.querySelectorAll(".menu-toggle"));
   const menu = document.getElementById("menu");
 
   if (menuButtons.length > 0) {
     menuButtons.forEach((menuButton) => {
       menuButton.addEventListener("click", (e) => {
         menu.classList.toggle("open");
+        document.body.classList.toggle("overflow-hidden");
+      });
+    });
+  }
+
+  const docsMenuButtons = [].slice.call(
+    document.querySelectorAll(".docs-menu-toggle")
+  );
+  const docsMenu = document.getElementById("docs-menu");
+
+  if (docsMenuButtons.length > 0) {
+    docsMenuButtons.forEach((docsMenuButton) => {
+      docsMenuButton.addEventListener("click", (e) => {
+        docsMenu.classList.toggle("open");
         document.body.classList.toggle("overflow-hidden");
       });
     });
