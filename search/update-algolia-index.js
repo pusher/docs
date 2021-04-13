66784047a5478489d55cb16d6d4b7d7e51d4093d
jonheslop <jon.heslop@pusher.com>
2021-04-13 13:48:14 +0100

    Handle promise

--- search/update-algolia-index.js
+++ search/update-algolia-index.js
@@ -6,8 +6,10 @@ if (VERCEL_ENV === "production") {
   const client = algoliasearch("BQ93464WAA", ALGOLIA_API_KEY);
   const index = client.initIndex("docs");
 
-  index.replaceAllObjects(records);
-  console.log("Algolia index updated");
+  index
+    .replaceAllObjects(records)
+    .then(() => console.log("Algolia index updated"));
+    .catch((error)=> console.error('Failed to Algolia update index', error));
 } else {
   console.log("Skipped Algolia update because this is not production");
 }
