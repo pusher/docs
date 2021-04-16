83a31176d73b05dd0409e1a1224d5d6f03807f94
jonheslop <jon.heslop@pusher.com>
2021-04-16 12:24:06 +0100

    Add default meta description

---
+++ _data/meta.js
@@ -0,0 +1,4 @@
+module.exports = {
+  description:
+    "Welcome to Pusher. Our hosted APIs are flexible, scalable, and easy to integrate. Get to know our products by getting started or exploring our API documentation.",
+};
