dece39f62d3e9173b797ddadc862f0fb5f89d780
jonheslop <jon.heslop@pusher.com>
2021-05-21 15:03:13 +0100

    Don’t need this now either

--- _data/meta.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  description:
-    "Welcome to Pusher. Our hosted APIs are flexible, scalable, and easy to integrate. Get to know our products by getting started or exploring our API documentation.",
-};
